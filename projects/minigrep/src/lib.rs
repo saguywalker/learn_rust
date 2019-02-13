d282efa3ed09319f02bdef2b11638115556ef0ec
saguywalker <guyguy252@gmail.com>
2019-02-13 17:46:31 +0700

    Add Config struct

---
+++ projects/minigrep/src/lib.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+fn parse_config(args: &[String]) -> (&str, &str){
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+}
+
+struct Config{
+    query: String,
+    filename: String,
+}
+
+impl Config{
+    fn new(args: &[String]) -> Config{
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config {query, filename}
+    }
 }
