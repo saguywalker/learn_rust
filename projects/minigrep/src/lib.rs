6108bb980ebffeb616e0746eda1a05808e22243c
saguywalker <guyguy252@gmail.com>
2019-02-13 20:27:53 +0700

    Add search_case_insensitive function

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -21,7 +21,7 @@ impl Config{
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
-    let results = search(&config.query, &contents);
+    let results = search_case_insensitive(&config.query, &contents);
     println!("With text:");
     for (i, result) in results.iter().enumerate(){
         println!("{}). {}", i,result)
@@ -35,6 +35,19 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
     
     for line in contents.lines(){
         if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&query){
             results.push(line);
         }
     }
@@ -57,6 +70,21 @@ Pick three.";
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -19,6 +19,8 @@ fn main() {
         println!("Application error: {}", e);
         process::exit(1);
     }
+
+    println!("********************************");
 }
 
 
