4f5930c0c1533983f365edf91d7e2b641a16f1fa
saguywalker <guyguy252@gmail.com>
2019-02-20 00:42:24 +0700

    Create Messenger trait and LimitTracker struct

---
+++ projects/LimitTracker/target/debug/deps/LimitTracker-29de2cc340f91124.d
@@ -0,0 +1,5 @@
+/home/sky/coding/learn_rust/projects/LimitTracker/target/debug/deps/LimitTracker-29de2cc340f91124.rmeta: src/main.rs
+
+/home/sky/coding/learn_rust/projects/LimitTracker/target/debug/deps/LimitTracker-29de2cc340f91124.d: src/main.rs
+
+src/main.rs:
---
+++ projects/LimitTracker/target/debug/deps/LimitTracker-a5d8b74fca73da25.d
@@ -0,0 +1,5 @@
+/home/sky/coding/learn_rust/projects/LimitTracker/target/debug/deps/LimitTracker-a5d8b74fca73da25.rmeta: src/lib.rs
+
+/home/sky/coding/learn_rust/projects/LimitTracker/target/debug/deps/LimitTracker-a5d8b74fca73da25.d: src/lib.rs
+
+src/lib.rs:
