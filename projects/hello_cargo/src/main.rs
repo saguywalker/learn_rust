8959893b61b08520b978d48a8e8d5499155a6feb
saguywalker <guyguy252@gmail.com>
2019-02-24 04:48:13 +0700

    Try TcpListener and TcpStream

--- projects/hello_cargo/src/main.rs
+++ projects/hello_cargo/src/main.rs
@@ -1,19 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+use std::fs;
 
-    let mut counter = 0;
+fn main(){
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    let result = loop{
-        counter += 1;
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
 
-        if counter == 10{
-            break counter * 2;
-        }
-    };
-
-    println!("Counter is {}", result);
-
-    for number in 2..5{
-        println!("{}!", number);
+        handle_connection(stream);
     }
 }
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK\r\n\r\n", "ok.html")
+    }else{
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
