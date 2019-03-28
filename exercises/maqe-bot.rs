8537f77428460c9793923dfc1cd07cb8fc6c6c64
saguywalker <guyguy252@gmail.com>
2019-03-28 22:29:53 +0700

    Finish Maqe-Bot

--- exercises/maqe-bot.rs
+++ exercises/maqe-bot.rs
@@ -1,42 +1,47 @@
 use std::env;
 
 fn main(){
-    let args: Vec<_> = env::args().skip(1).collect();
-    if args.len() == 0{
+    let args: Vec<_> = env::args().collect();
+    if args.len() == 1{
         println!("Error! Please provide command!");
         return;
     }
 
-    let mut direction = 1;
-    let mut x = 0u32;
-    let mut y = 0u32;
+    const DIRECTION: [&'static str; 4] = ["East", "North", "West", "South"];
+    let mut direction: i32 = 1;
+    let mut x = 0i32;
+    let mut y = 0i32;
 
     let mut i: usize = 0;
-    let command: Vec<_> = args[0].chars().collect();
+    let command: Vec<_> = args[1].chars().collect();
     while i < command.len(){
         match command[i]{
             'W' => {
                 let (num_size, num_value) = check_num(&command, i+1);
 
-                
+                if direction % 2 == 0{
+                    x = x + num_value as i32 * (1 - direction) as i32;
+                }else{
+                    y = y + num_value as i32 * (2 - direction) as i32;
+                }
 
-                println!("Walk {}!", num_value);
                 i = i + num_size + 1;
                 continue
             },
-            'R' => println!("Turn Right!"),
-            'L' => println!("Turn Left!"),
+            'R' => direction = sub_mod(direction, 1, 4),
+            'L' => direction = (direction + 1) % 4,
             x => println!("Something: {}", x),
         }
         i += 1;
     }
+
+    println!("X: {}, Y: {}, Direction: {}", x, y, DIRECTION[direction as usize]);
 
 }
 
 fn check_num(cmd: &Vec<char>, idx: usize) -> (usize, u32){
     let mut i = 0;
     let mut value = String::new();
-    let mut num_chars: Vec<char> = Vec::new();
     while i < cmd.len() - idx{
         match cmd[idx+i].to_digit(10){
             Some(n) => {
@@ -48,4 +53,13 @@ fn check_num(cmd: &Vec<char>, idx: usize) -> (usize, u32){
     }
 
     (i, value.parse().unwrap())
+}
+
+fn sub_mod(a: i32, b: i32, m: u32) -> i32{
+    let mut result = a - b;
+    if result < 0{
+        result = m as i32 + result;
+    }
+
+    result
 }
