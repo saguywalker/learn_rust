2d63233db300dd8f31d6c1bced2766a08acd5a38
saguywalker <guyguy252@gmail.com>
2019-07-08 11:51:26 +0300

    add sieve

--- exercises/algorithm/src/main.rs
+++ exercises/algorithm/src/main.rs
@@ -1,7 +1,7 @@
 pub mod josephus;
 pub mod problem1;
 
-use problem1::{sum, dedup, filter};
+use problem1::{sum, dedup, filter, Matrix, mat_mul, sieve};
 
 fn main() {
     josephus::josephus(2, 7);
@@ -9,4 +9,10 @@ fn main() {
     println!("{:?}", dedup(&vec![1,2,2,3,4,5,6,7,7]));
     let even_filtered = filter(&vec![1,2,3,4,5,6], &|i: i32| -> bool {i % 2 ==0});
     println!("{:?}", even_filtered);
+    let mat1: Matrix = vec![vec![3.0,4.0,2.0]];
+    let mat2: Matrix = vec![vec![13.0, 19.0, 7.0, 15.0],
+                            vec![8.0, 7.0, 4.0, 6.0],
+                            vec![6.0, 4.0, 0.0, 3.0]];
+    println!("{:?}", mat_mul(&mat1, &mat2));
+    println!("{:?}", sieve(25));
 }
--- exercises/algorithm/src/problem1.rs
+++ exercises/algorithm/src/problem1.rs
@@ -26,12 +26,63 @@ pub fn filter(vs: &Vec<i32>, pred: &Fn(i32) -> bool) -> Vec<i32>{
     result
 }
 
+//matrix multiplication
 pub type Matrix = Vec<Vec<f32>>;
 
-pub fn mat_mut(mat1: &Matrix, mat2: &Matrix) -> Matrix{
+pub fn mat_mul(mat1: &Matrix, mat2: &Matrix) -> Matrix{
     if mat1[0].len() != mat2.len(){
         panic!("Columns of mat1 must equal to Rows of mat2");
     }
+    let mut mat3: Matrix = vec![vec![0.0; mat2[0].len()]; mat1.len()];
+    for i in 0..mat1.len(){
+        for j in 0..mat2[0].len(){
+            for k in 0..mat2.len(){
+                mat3[i][j] += mat1[i][k] * mat2[k][j]
+            }
+        }
+    }
+    mat3
+}
+
+//sieve of eratosthenes
+pub fn sieve(n: u32) -> Vec<u32>{
+    let mut result: Vec<u32> = Vec::new();
+
+    let mut is_prime = vec![true; n as usize + 1];
+    is_prime[0] = false;
+    is_prime[1] = false;
     
+    let mut even = 2 * 2;
+    while even < n{
+        is_prime[even as usize] = false;
+        even += 2;
+    }
+    result.push(2);
+    
+    for i in 3..=n{
+        if is_prime[i as usize]{
+            let mut tmp = i * i;
+            while tmp < n{
+                is_prime[tmp as usize] = false;
+                tmp *= 2;
+            }
+            result.push(i);
+        }
+    }
+
+    result     
+}
+
+//tower of hanoi
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum Peg{
+    A,
+    B,
+    C,
+}
+
+pub type Move = (Peg, Peg);
+
+pub fn hanoi(num_discs: u32, src: Peg, aux: Peg, dist: Peg) -> Vec<Move>{
     unimplemented!();
 }
